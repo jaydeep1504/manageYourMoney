2884121c9f24b4da9b09ceb1edafc86291709a5d
Jaydeep Paul <jaydeep.paul83@gmail.com>
2023-07-08 15:15:43 +0530

    Initialise Manage Your Money

---
+++ src/main/java/com/app/manageYourMoney/ManageYourMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.app.manageYourMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageYourMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageYourMoneyApplication.class, args);
+	}
+
+}
